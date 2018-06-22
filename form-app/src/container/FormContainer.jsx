7f40f0ac721d373ae4bc7791100590010c2dff79
somnath46 <4684.somnath@gmail.com>
2018-06-22 18:46:09 +0530

    form-app commit

--- form-app/src/container/FormContainer.jsx
+++ form-app/src/container/FormContainer.jsx
@@ -7,5 +7,44 @@ import TextArea from "../components/TextArea";
 import Select from "../components/Select";
 
 class FormContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newUser: {
+                name: "",
+                age: "",
+                gender: "",
+                skills: [],
+                about: ""
+            },
+            genderOptions: ["Male", "Female", "Others"],
+            skillOptions: ["Programming", "Development", "Design", "Testing"] 
+        }       
+    }
 
+    handleFullName(e) {
+        let value = e.target.value;
+        this.setState(
+          prevState => ({
+            newUser: {
+              ...prevState.newUser,
+              name: value
+            }
+          }),
+          () => console.log(this.state.newUser)
+        );
+    }
+
+    handleAge(e) {
+        let value = e.target.value;
+        this.state(
+            prevState => ({
+                newUser: {
+                    ...prevState.newUser,
+                    age: value
+                }
+            }),
+            () => console.log(this.state.newUser)
+        );
+    }
 }
